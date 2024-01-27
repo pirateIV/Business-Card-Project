ba58038a362ab6980a5aa8698ea5051fa7df62c5
ben64103 <benabolade@gmail.com>
2024-01-28 00:07:07 +0100

    made changes

--- src/assets/components/Card/common/ContactInfo.jsx
+++ src/assets/components/Card/common/ContactInfo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ContactInfo = ({ id, type, icon,href, contactMethod }) => {
+const ContactInfo = ({ id, type, icon, href, contactMethod }) => {
   return (
     <>
       <section className='profile flex flex-col gap-3 w-64' id={id}>
@@ -8,7 +8,13 @@ const ContactInfo = ({ id, type, icon,href, contactMethod }) => {
           {icon}
           <h4 className='font-semibold'>{type}</h4>
         </div>
-        <a className='font-[300] text-white__87 text-[16px]' href={href}>{contactMethod}</a>
+        <a
+          className='font-[300] text-white__87 text-[16px]'
+          href={href}
+          target='_blank'
+          rel='noreferrer'>
+          {contactMethod}
+        </a>
       </section>
     </>
   );
