8a12e2dd1b2fa67750384e5fdda04e2358514abd
ben64103 <benabolade@gmail.com>
2024-01-28 06:26:57 +0100

    done with the user-image section

--- src/assets/components/Card/Image/UserProfileImage.jsx
+++ src/assets/components/Card/Image/UserProfileImage.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const UserProfileImage = () => {
   return (
-    <img src="../../images/user-photo.jpg" alt="user profile image" />
-  )
-}
+    <div className='absolute z-50 w-[340px] h-[549px] top-[40px] start-[280px]'>
+      <img src='/images/user-photo.jpg' alt='user profile image' className='w-full h-full object-cover' />
+    </div>
+  );
+};
 
-export default UserProfileImage
+export default UserProfileImage;
